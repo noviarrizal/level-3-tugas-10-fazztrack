77a326cf8bfe9f88bc87945b746409774e64930a
noviarrizal <noviarrizalarif@gmail.com>
2021-10-06 15:08:05 +0700

    Edit Tabel ID

--- index.php
+++ index.php
@@ -4,39 +4,42 @@ $produk = query("SELECT * FROM produk");
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Data Produk</title>
 </head>
+
 <body>
 
-<h1>Halaman Data Produk</h1>
+	<h1>Halaman Data Produk</h1>
 
-<a href="tambah.php">Tambah Data Produk</a>
-<br><br>
+	<a href="tambah.php">Tambah Data Produk</a>
+	<br><br>
 
-<table border="1" cellspacing="0" cellpadding="5">
-	<tr>
-		<th>#</th>
-		<th>Nama Produk</th>
-		<th>Keterangan</th>
-		<th>Harga</th>
-		<th>Jumlah</th>
-		<th>Aksi</th>
-	</tr>
-	<?php $i = 1; ?>
-	<?php foreach( $produk as $row ) { ?>
-	<tr>
-		<td><?= $i; ?></td>
-		<td><?= $row["nama_produk"]; ?></td>
-		<td><?= $row["keterangan"]; ?></td>
-		<td><?= $row["harga"]; ?></td>
-		<td><?= $row["jumlah"]; ?></td>
-        <td><a href="ubah.php?id=<?php echo $row["id"]; ?>">ubah</a> | <a href="hapus.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('yakin?')">hapus</a></td>
-	</tr>
-	<?php $i++; ?>
-	<?php } ?>
-</table>
+	<table border="1" cellspacing="0" cellpadding="5">
+		<tr>
+			<th>ID</th>
+			<th>Nama Produk</th>
+			<th>Keterangan</th>
+			<th>Harga</th>
+			<th>Jumlah</th>
+			<th>Aksi</th>
+		</tr>
+		<?php $i = 1; ?>
+		<?php foreach ($produk as $row) { ?>
+			<tr>
+				<td><?= $i; ?></td>
+				<td><?= $row["nama_produk"]; ?></td>
+				<td><?= $row["keterangan"]; ?></td>
+				<td><?= $row["harga"]; ?></td>
+				<td><?= $row["jumlah"]; ?></td>
+				<td><a href="ubah.php?id=<?php echo $row["id"]; ?>">ubah</a> | <a href="hapus.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('yakin?')">hapus</a></td>
+			</tr>
+			<?php $i++; ?>
+		<?php } ?>
+	</table>
 
 
 </body>
+
 </html>
